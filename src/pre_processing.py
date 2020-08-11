9ec734628ad4a78adb330ec45bd61cf257228b19
Kyle Manning <52970594+MrManning@users.noreply.github.com>
2020-08-11 14:37:37 +0100

    added tokenization and tagging for preprocessing

--- src/pre_processing.py
+++ src/pre_processing.py
@@ -1,3 +1,8 @@
+import nltk
+
+from src.helpers import remove_extra_spaces
+
+
 def pre_processing(html):
     """Minimises the HTML into individual sentences removing additional whitespace, then word tokens in lowercase form
 
@@ -6,8 +11,20 @@ def pre_processing(html):
     """
     stripped = remove_extra_spaces(html)
 
-    return html
+    # Creates a list for every variable using a loop
+    sent_list, token_list, pos_list = ([] for _ in range(3))
 
+    # For loop tokenizes the html into sentences
+    for sentences in nltk.sent_tokenize(stripped):
+        sent_list.append(sentences)
 
-def remove_extra_spaces(text):
-    return ' '.join(text.split())
+        # For loop tokenizes sentences into words
+        words = [x.lower() for x in nltk.word_tokenize(sentences.replace("/", " "))]
+        for token in words:
+            token_list.append(token)
+
+        # Creates part of speech tags for every word
+        pos_list.extend(nltk.pos_tag(words))
+
+    return token_list, pos_list
+
