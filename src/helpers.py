20dd42aa181ee17bf55ba2fcea9c8baedb2fc42d
Kyle Manning <52970594+MrManning@users.noreply.github.com>
2020-08-13 17:55:31 +0100

    added function to remove punctuation and a test

--- src/helpers.py
+++ src/helpers.py
@@ -1,5 +1,36 @@
+import nltk
+import re
+import string
+
+punct = set(string.punctuation)
+punct.update(['...', '£', '•'])
+punct = ''.join(punct)
+
+stopwords = set(nltk.corpus.stopwords.words('english'))
+
+
 def remove_extra_spaces(text):
     return ' '.join(text.split())
+
+
+def remove_punctuation(word_list, word_list_type="list"):
+    # regex = re.compile("[%s]" % re.escape(punct))
+    regex = re.compile("^([%s]|([%s]+))$" % (re.escape(punct), re.escape(punct)))
+
+    if word_list_type == "list":
+        punctuation_less = [regex.sub('', term) for term in word_list if regex.sub('', term)]
+    else:
+        punctuation_less = [(regex.sub('', term), tag) for term, tag in word_list if regex.sub('', term)]
+
+    return punctuation_less
+
+
+def remove_digits(word_list):
+    return False
+
+
+def remove_stopwords(word_list):
+    return False
 
 
 def write_to_file():
--- tests/test_helpers.py
+++ tests/test_helpers.py
@@ -1,25 +1,36 @@
-from src.helpers import remove_extra_spaces
+from collections import Counter
+from src.helpers import remove_extra_spaces, remove_punctuation
+
+accepted_string = "This is a test example"
 
 
-class TestRemoveExtraSpaces:
-    accepted_string = "This is a test example"
+def test_multiple_spaces(self):
+    multiple_spaces = "This  is a    test   example"
+    res = remove_extra_spaces(multiple_spaces)
+    assert self.accepted_string == res
 
-    def test_multiple_spaces(self):
-        multiple_spaces = "This  is a    test   example"
-        res = remove_extra_spaces(multiple_spaces)
-        assert self.accepted_string == res
 
-    def test_multiple_newlines(self):
-        multiple_spaces = "This    \n is a test \n \n example"
-        res = remove_extra_spaces(multiple_spaces)
-        assert self.accepted_string == res
+def test_multiple_newlines(self):
+    multiple_spaces = "This    \n is a test \n \n example"
+    res = remove_extra_spaces(multiple_spaces)
+    assert self.accepted_string == res
 
-    def test_multiple_tabs(self):
-        multiple_spaces = "This \t\t is a \t test \t example"
-        res = remove_extra_spaces(multiple_spaces)
-        assert self.accepted_string == res
 
-    def test_multiple_whitespace_characters(self):
-        multiple_spaces = "This \t is \n     a \t test \n example"
-        res = remove_extra_spaces(multiple_spaces)
-        assert self.accepted_string == res
+def test_multiple_tabs(self):
+    multiple_spaces = "This \t\t is a \t test \t example"
+    res = remove_extra_spaces(multiple_spaces)
+    assert self.accepted_string == res
+
+
+def test_multiple_whitespace_characters(self):
+    multiple_spaces = "This \t is \n     a \t test \n example"
+    res = remove_extra_spaces(multiple_spaces)
+    assert self.accepted_string == res
+
+
+def test_remove_punctuation():
+    multiple_punct = [".", "a", "£30", "{", "t!st", "{example}"]
+    res = remove_punctuation(multiple_punct)
+    accepted_list = ["a", "{example}", "£30", "t!st"]
+
+    assert Counter(accepted_list) == Counter(res)
