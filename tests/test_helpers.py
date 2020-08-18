a673921f1c4b877a4b234682c3436576a6ff2422
Kyle Manning <52970594+MrManning@users.noreply.github.com>
2020-08-18 15:52:05 +0100

    fixed helper tests assertion order from expected then actual to actual then expected
    added tests for helper functions

--- tests/test_helpers.py
+++ tests/test_helpers.py
@@ -1,36 +1,56 @@
 from collections import Counter
-from src.helpers import remove_extra_spaces, remove_punctuation
+from src.helpers import remove_extra_spaces, remove_punctuation, remove_digits
 
 accepted_string = "This is a test example"
 
 
 def test_multiple_spaces():
     multiple_spaces = "This  is a    test   example"
-    res = remove_extra_spaces(multiple_spaces)
-    assert accepted_string == res
+    result = remove_extra_spaces(multiple_spaces)
+
+    assert result == accepted_string
 
 
 def test_multiple_newlines():
     multiple_spaces = "This    \n is a test \n \n example"
-    res = remove_extra_spaces(multiple_spaces)
-    assert accepted_string == res
+    result = remove_extra_spaces(multiple_spaces)
+
+    assert result == accepted_string
 
 
 def test_multiple_tabs():
     multiple_spaces = "This \t\t is a \t test \t example"
-    res = remove_extra_spaces(multiple_spaces)
-    assert accepted_string == res
+    result = remove_extra_spaces(multiple_spaces)
+
+    assert result == accepted_string
 
 
 def test_multiple_whitespace_characters():
     multiple_spaces = "This \t is \n     a \t test \n example"
-    res = remove_extra_spaces(multiple_spaces)
-    assert accepted_string == res
+    result = remove_extra_spaces(multiple_spaces)
+
+    assert result == accepted_string
 
 
 def test_remove_punctuation():
     multiple_punctuation = [".", "a", "£30", "{", "t!st", "{example}"]
-    res = remove_punctuation(multiple_punctuation)
+    result = remove_punctuation(multiple_punctuation)
     accepted_list = ["a", "{example}", "£30", "t!st"]
 
-    assert Counter(accepted_list) == Counter(res)
+    assert Counter(result) == Counter(accepted_list)
+
+
+def test_remove_digits():
+    multiple_digits = ["30", "2004-12", "a number 42", "test", "empty", "2020"]
+    result = remove_digits(multiple_digits)
+    accepted_list = ["2004-12", "a number 42", "empty", "test"]
+
+    assert Counter(result) == Counter(accepted_list)
+
+
+def test_remove_stopwords():
+    multiple_stopwords = ["the", "test", "example", "and"]
+    result = remove_stopwords(multiple_stopwords)
+    accepted_list = ["test", "example"]
+
+    assert Counter(result) == Counter(accepted_list)
